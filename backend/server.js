bd31e9c9085719ca8ef4bb3f8163875d2fc56a45
agus10c <acaceresrapetti@gmail.com>
2025-10-04 20:17:10 -0300

    primer avance de la interfaz de usuario

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('./mi-logger');
 const productosRoutes = require('./routes/productosRoutes.js');
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
